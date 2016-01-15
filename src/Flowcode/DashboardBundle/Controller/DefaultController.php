6e2f1b58d0b41b29eb465318f94e90c04c3ca34e
Pedro <pbarri@flowcode.com.ar>
2016-01-15 16:56:26 -0300

    Disponibles las opciones habladas. Se mejoro el dashboard con box usados. Link de logo.

--- src/Flowcode/DashboardBundle/Controller/DefaultController.php
+++ src/Flowcode/DashboardBundle/Controller/DefaultController.php
@@ -21,11 +21,24 @@ class DefaultController extends Controller {
      * @Method("GET")
      * @Template()
      */
-    public function indexAction() {
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $products = $em->getRepository('AmulenShopBundle:Product')->findAll();
+        $countries = $em->getRepository('AppBundle:Country')->findAll();
+        $industries = $em->getRepository('AppBundle:Industry')->findAll();
+
+        $qtyProducts = count($products);
+        $qtyCountries = count($countries);
+        $qtyIndustries = count($industries);
 
         $today = date("d/m/Y H:m");
+
         return $this->render('FlowcodeDashboardBundle:Default:index.html.twig', array(
             "datime" => $today,
+            "qtyProducts" => $qtyProducts,
+            "qtyCountries" => $qtyCountries,
+            "qtyIndustries" => $qtyIndustries,
         ));
     }
 
