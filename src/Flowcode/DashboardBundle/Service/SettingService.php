47910dc0a5b19a623508dbc52debee50a9207eed
Juan Manuel Aguero <juanma.aguero@gmail>
2016-05-28 18:14:51 -0300

    Plugins

---
+++ src/Flowcode/DashboardBundle/Service/SettingService.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Flowcode\DashboardBundle\Service;
+
+/**
+ * User: juanma
+ * Date: 5/28/16
+ * Time: 6:03 PM
+ */
+class SettingService
+{
+
+    public function currentVersion()
+    {
+        return "0.4.0";
+    }
+
+}
